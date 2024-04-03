3c9a175a82e5114137775eca87d866641dde45af
samanthfel <143027804+samanthfel@users.noreply.github.com>
2024-04-03 12:03:38 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -12,7 +12,7 @@ public class WordList
   {
     int count=0;
     for (int i=0;i<myList.size();i++){
-      if(myList.get(i).size()==len)
+      if(myList.get(i).length()==len)
         count++;
     }return count; 
   }
@@ -20,7 +20,7 @@ public class WordList
   public void removeWordsOfLength(int len)
   {
     for (int i=0;i<myList.size();i++){
-      if(myList.get(i).size()==len){
+      if(myList.get(i).length()==len){
         myList.remove(i);
         i--;
       }
